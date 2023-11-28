85ab3be14403f96fe98af25b972e64a07cd9b288
AlaaYaish7 <152174917+AlaaYaish7@users.noreply.github.com>
2023-11-28 13:27:16 +0200

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,16 @@
-const express= require('express')
+const express=require('express')
+const reportcontroller=require("../controllers/ReportsController")
+const router=require ('express').Router();
 
-const router=require('express').Router();
+/*router.get("/",(req,res,next)=>
+{
+    res.send("alaa")
+})*/
 
-router.get("/",(req,res,next)=>{res.send("farah")})
+
+    router.get("/allreports",reportcontroller.getallreports)
+    router.post("/addreport",reportcontroller.addnewreport)
+    router.post("/deletereport",reportcontroller.deletereport)
+    router.post("/editreport",reportcontroller.updatereport)
 
 module.exports=router
