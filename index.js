672cb0aa9ca51fc80bc7c9a947933e0072241f59
AlaaYaish7 <152174917+AlaaYaish7@users.noreply.github.com>
2023-11-28 13:28:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express=require('express')
-
 const mydb=require('./config/db')
-
-
 const app=express();
 const rout=require("./routes/router")
+const bodyparser=require("body-parser")
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended:true}))
 app.use(rout)
 
-app.get("/",(req,res,next)=>{r
-    es.send("farah")})
-app.listen(3001,()=>{
-    console.log('server is run ');
+
+
+app.listen(8000,()=>{
+    console.log('server is running');
+
 })
