d75a8077d0927819b9543cb8eb837239a1dfed62
farahdilea <152026803+farahdilea@users.noreply.github.com>
2023-11-29 17:52:25 +0200

    Update Data.js

--- models/Data.js
+++ models/Data.js
@@ -17,7 +17,23 @@ class DataModel {
         throw new Error(error.message);
       }
     } 
+    
+    static async addData(id,userid,air_Q,water_Q,humid,tempr,winds,sco,resou){
 
+return new Promise (resolve=> { db.query("INSERT INTO posts(post_id, user_id, air_quality, water_quality, humidity, temp, wind_speed, score, resource)VALUES (?,?,?,?,?,?,?,?,?)",
+[id,userid,air_Q,water_Q,humid,tempr,winds,sco,resou], (error, result) => {
+         if (!error) 
+           resolve(true)
+          else 
+           resolve(false)
+         
+
+       } )
+      })
+
+
+    
+} 
 } 
 
 
