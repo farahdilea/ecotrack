012e867ff3f8169a5c58b6b26b46de4738ed67cc
farahdilea <152026803+farahdilea@users.noreply.github.com>
2023-12-24 21:54:00 +0200

    Update resources.js

--- models/resources.js
+++ models/resources.js
@@ -20,6 +20,23 @@ class resourceModel {
       } 
 //------------------------------------------------------------------
 
+static async getmyResource(topic){
+  try {
+    const result = await new Promise((resolve, reject) => {
+      db.query("SELECT * FROM `resources` WHERE `topic`=?", [topic], (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+
+} 
 
 }
 module.exports=resourceModel
