1a790f27771f20d6a92a7d4a97b7fc2234c5cc1c
farahdilea <152026803+farahdilea@users.noreply.github.com>
2023-12-27 10:02:58 +0200

    Update resources.js add function addresources

--- models/resources.js
+++ models/resources.js
@@ -37,6 +37,39 @@ static async getmyResource(topic){
   }
 
 } 
+//--------------------------------------------------------------------
+static async addResource(research_id,user_id, topic,links,desc){
+  try {
+    const result = await new Promise((resolve, reject) => {
+      db.query("SELECT `type` FROM `user` WHERE `user_id`=?", [user_id], (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+
+    if (result && result.length > 0 && result[0].type === 1) {
+      return new Promise((resolve) => {
+        db.query("INSERT INTO `resources`(`research_id`, `user_id`, `topic`, `links`, `description`) VALUES (?,?,?,?,?)",
+          [research_id, user_id, topic, links, desc], (error, result) => {
+            if (!error) {
+              resolve(true);
+            } else {
+              resolve(false);
+            }
+          });
+      });
+    } else {
+      console.log("You can't add resource."); // Log the message if the user isn't authorized
+      return false; // Indicate the failure to add resource due to unauthorized user
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    return false; // Return false to indicate an error occurred
+  }
+}
 
 }
 module.exports=resourceModel
